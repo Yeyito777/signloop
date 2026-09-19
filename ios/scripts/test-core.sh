bad58feb4f63a9f8f74925a1a0ad8c0e5887daf1
Eldiiar Bekbolotov <eldiiarbekbolotov@gmail.com>
2026-09-19 15:19:03 -0400

    port features segmenter and engine to swift

--- ios/scripts/test-core.sh
+++ ios/scripts/test-core.sh
@@ -11,3 +11,5 @@ swiftc -parse-as-library Signloop/Recognition.swift Signloop/LiveWindowPolicy.sw
 "$binary"
 swiftc -parse-as-library Signloop/CaptureFreshness.swift Signloop/CaptureCadence.swift Tests/CaptureFreshnessTests.swift -o "$binary"
 "$binary"
+swiftc -parse-as-library Signloop/Recognition.swift Signloop/SignEngineFeatures.swift Signloop/SignSegmenter.swift Signloop/SignEngine.swift Tests/SignEngineParity.swift -o "$binary"
+"$binary" Tests/Fixtures/sign_engine_golden.json
